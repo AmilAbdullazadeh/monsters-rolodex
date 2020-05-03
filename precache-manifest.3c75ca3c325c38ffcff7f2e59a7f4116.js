40e9b51c1d31fa13f4a55200dc88bccf98235f9a
Amil <amilva>
2020-05-03 19:41:41 +0400

    Updates

---
+++ precache-manifest.3c75ca3c325c38ffcff7f2e59a7f4116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae46fbed96598089b0f0dd8310389fd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eb998d1ed7c42fe80012",
+    "url": "/monsters-rolodex/static/css/main.c7815b40.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb998d1ed7c42fe80012",
+    "url": "/monsters-rolodex/static/js/main.e205e5a1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
